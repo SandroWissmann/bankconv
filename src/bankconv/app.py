b8354017b93c27fd8a00793ef75af95c64873d02
SandroWissmann <sandro.wissmann@gmail.com>
2021-04-27 10:25:00 +0200

    feat: adds iteration over pdf files in selected folder

--- src/bankconv/app.py
+++ src/bankconv/app.py
@@ -4,17 +4,23 @@
 import tkinter as tk
 import tkinter.filedialog
 
+import os
+
 
 def main():
+    # get directory
     root = tk.Tk()
     root.withdraw()
-    folder_selected = tk.filedialog.askdirectory()
+    directory = tk.filedialog.askdirectory()
 
-    print(folder_selected)
+    print(directory)
 
-    # gui to get folder?
+    # iterate over all pdf files in folder
+    for filename in os.listdir(directory):
+        if filename.lower().endswith(".pdf"):
+            print(os.path.join(directory, filename))
 
-    # iterate over all pdfs in folder
+    # read out data from each pdf file. check if it is a table already?
 
     # create attach each pdf to an excel file going by year
 
