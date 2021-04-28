b453de26d60d8a296e315d45bace59f27571da18
SandroWissmann <sandro.wissmann@gmail.com>
2021-04-28 18:07:48 +0200

    feat: adds readout of raw txt data from file

--- src/bankconv/app.py
+++ src/bankconv/app.py
@@ -1,26 +1,77 @@
 #!/usr/bin/env python3
 
-# from tkinter import filedialog
+from typing import List
+
 import tkinter as tk
 import tkinter.filedialog
 
 import os
 
+from tika import parser
+
+
+def getDirectory() -> str:
+    """
+    Select directory from GUI User input and return it.
+    """
+    # get directory
+    # root = tk.Tk()
+    # root.withdraw()
+    # directory = tk.filedialog.askdirectory()
+    directory = "/mnt/Buisness/Haushalt/Kreditkartenabrechnungen Sandro"
+    return directory
+
+
+def getTextLinesFromPDF(directory: str, filename_pdf: str) -> List[str]:
+    """
+    Extract Text from PDF file.
+    Requires input to be a pdf file.
+    Returns dict with lines of text in PDF file.
+    """
+    filename_pdf_absolut: str = os.path.join(directory, filename_pdf)
+
+    raw: str = str(parser.from_file(filename_pdf_absolut))
+    text: str = str(raw.encode("utf-8", errors="ignore"))
+    return text.split("\\n")
+
+
+def getFilenameTxtAbsolut(directory: str, filename_pdf: str) -> str:
+    """
+    Get txt filename absolut based on pdf filename.
+    """
+    filename_txt: str = os.path.splitext(filename_pdf)[0] + ".txt"
+    filename_txt_absolut: str = os.path.join(directory, filename_txt)
+    return filename_txt_absolut
+
+
+def writeTextLinesToFileInDir(
+    filename_txt_absolut: str, text_lines: List[str]
+) -> None:
+    """
+    Write text lines to file.
+    """
+    text_file: file = open(filename_txt_absolut, "w")
+    for text_line in text_lines:
+        text_file.write(text_line)
+        text_file.write("\n")
+    text_file.close()
+
 
 def main():
-    # get directory
-    root = tk.Tk()
-    root.withdraw()
-    directory = tk.filedialog.askdirectory()
-
-    print(directory)
-
     # iterate over all pdf files in folder
-    for filename in os.listdir(directory):
-        if filename.lower().endswith(".pdf"):
-            print(os.path.join(directory, filename))
+    directory: str = getDirectory()
 
-    # read out data from each pdf file. check if it is a table already?
+    for filename_pdf in os.listdir(directory):
+        if not filename_pdf.lower().endswith(".pdf"):
+            continue
+
+        text_lines: List[str] = getTextLinesFromPDF(directory, filename_pdf)
+
+        filename_txt_absolut: str = getFilenameTxtAbsolut(
+            directory, filename_pdf
+        )
+
+        writeTextLinesToFileInDir(filename_txt_absolut, text_lines)
 
     # create attach each pdf to an excel file going by year
 
